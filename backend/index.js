eec7799c38060463d81d978ae6a43563ada9404e
Asad-ali349 <asadking066@gmail.com>
2023-11-24 12:27:53 +0500

    cors

--- backend/index.js
+++ backend/index.js
@@ -82,9 +82,21 @@ app.get("/", (req, res) => {
 });
 
 // Connecting with MongoDB with the mongoose.connect() method.
-mongoose.connect(process.env.CONNECTION_URL, CONNECTION_PARAMS)
-  .then(() => app.listen(PORT, console.log(`Server running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+// mongoose.connect(process.env.CONNECTION_URL, CONNECTION_PARAMS)
+//   .then(() => app.listen(PORT, console.log(`Server running on port: ${PORT}`)))
+//   .catch((error) => console.log(error.message));
+
+
+  mongoose.connect(process.env.CONNECTION_URL , {useNewUrlParser: true,useUnifiedTopology: true}) 
+ .then(
+  () => app.listen(PORT, console.log(`Server running on port: ${PORT}`))
+ )
+  .catch((err) => {
+    console.log("Mongodb connection error", err);
+  });
+
+
+
 
 
 export default app;
